13c996787312534080ed9adc6ca1933f796e332c
Manikandan <57445267+manikandan71@users.noreply.github.com>
2021-05-13 15:40:32 +0530

    update server.js

--- demos/browser/server.js
+++ demos/browser/server.js
@@ -12,7 +12,7 @@ const { v4: uuidv4 } = require('uuid');
 const meetingTable = {};
 
 // Use local host for application server
-const host = '127.0.0.1:8081';
+const host = '127.0.0.1:3000';
 
 // Load the contents of the web application to be used as the index page
 const indexPage = fs.readFileSync(`dist/${process.env.npm_config_app || 'meetingV2'}.html`);
